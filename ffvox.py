3790eaab39576fa3e56261032b1f135e2794884a
indieleet <indieleet@gmail.com>
2024-01-13 00:49:06 +0300

    first working example

--- ffvox.py
+++ ffvox.py
@@ -1,25 +1,42 @@
-import os
+from os import system
 from shutil import which
-class FFVox:
+
+class Tracker:
     def __init__(self):
         self.raw_exp = []
+        self.raw_pattern = []
+        self.raw_inst = []
         self.start = "ffmpeg "
+        self.aeval_init = "-f lavfi -i \"aevalsrc=\' "
         self.overwrite = True
-        self.main_str = (f"{self.start}"
-        "-y -f lavfi -i " 
-        "\"aevalsrc=\'\ "
-        "0.5*mod(t,1)*lt(mod(t,2),1)*((0.25)-mod(t,(0.25)))*(-1+random(0)*2)+\
-        ""(0.5)*gt(t,2)*lt(t,4)*sin(2*PI*t*330)+"
-        "(0.3)*gt(t,3)*sin(2*PI*t*440)+"
-        "(0.5)*gt(t,4)*sin(2*PI*t*220+(sin(2*PI*t*220)*0.5*((0.5)-mod(t,(0.5)))))+"
-        "(0.25)*lt(t,1.75)*mod(t,1/330)*330-0.5\':"
-        "d=5\" "
-        "out.wav")
+        self.duration = -1
+        self.end = "out.wav"
+          
+    def add_pattern(self, pattern: list):
+        self.raw_pattern.append(pattern)
+    def add_inst(self, inst: str):
+        self.raw_inst.append(inst)
     def __eval(self):
-        os.system(self.main_str)
+        overwrite_tag = ""
+        if self.overwrite:
+            overwrite_tag = "-y "
+        expr_inst = "".join(["+".join(self.raw_inst), "\':"])
+        duration = 0
+        if self.duration == -1:
+            #TBD: duration = lenght of track
+            pass
+        elif self.duration > 0:
+            duration = self.duration
+        expr = (f"{self.start}"
+                f"{overwrite_tag}"
+                f"{self.aeval_init}"
+                f"{expr_inst}"
+                f"d={duration}\" "
+                f"{self.end}")
+        system(expr)
     def render(self):
         self.__eval()
     def play(self):
         self.__eval()
         if which("termux-media-player"):
-            os.system("termux-media-player play out.wav")
+            system("termux-media-player play out.wav")
--- simple.py
+++ simple.py
@@ -1,3 +1,5 @@
-from ffvox import FFVox
-
-FFVox().play()
+from ffvox import Tracker 
+tracker = Tracker()
+tracker.add_inst("0.5*mod(t,1)*lt(mod(t,2),1)*((0.25)-mod(t,(0.25)))*(-1+random(0)*2)")
+tracker.duration = 3
+tracker.play()
