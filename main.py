4dc7df65eac2e57f8e00649da4ffcce1c35c7834
indieleet <indieleet@gmail.com>
2024-01-13 00:06:59 +0300

    basic impl

--- main.py
+++ main.py
@@ -1,30 +1,40 @@
 import os
 from shutil import which
 
-class FFVox:
+class Tracker:
     def __init__(self):
         self.raw_exp = []
         self.raw_pattern = []
         seld.raw_inst = []
         self.start = "ffmpeg "
+        self.aeval_init = "-f lavfi -i \"aevalsrc=\' "
         self.overwrite = True
         self.duration = -1
         self.end = "out.wav"
-        self.main_str = (f"{self.start} "
-        "-f lavfi -i \"aevalsrc=\' "
-        "0.5*mod(t,1)*lt(mod(t,2),1)*((0.25)-mod(t,(0.25)))*(-1+random(0)*2)+"
-        "(0.5)*gt(t,2)*lt(t,4)*sin(2*PI*t*330)+"
-        "(0.3)*gt(t,3)*sin(2*PI*t*440)+"
-        "(0.5)*gt(t,4)*sin(2*PI*t*220+(sin(2*PI*t*220)*0.5*((0.5)-mod(t,(0.5)))))+"
-        "(0.25)*lt(t,1.75)*mod(t,1/330)*330-0.5\':"
-        "d=5\" "
-        f"{self.end}")
+          
     def add_pattern(self, pattern: list):
         self.raw_pattern.append(pattern)
     def add_inst(self, inst: str):
         self.raw_inst.append(inst)
     def __eval(self):
-        os.system(self.main_str)
+        overwrite_tag = ""
+        if self.overwrite:
+            overwrite_tag = "-y "
+        expr_inst = "".join("+".join(self.raw_inst), "/' ")
+        duration = 0
+        if self.duration == -1:
+            #TBD: duration = lenght of track
+            pass
+        elif self.duration > 0:
+            duration = self.duration
+        expr = (f"{self.start}"
+                f"{overwrite_tag}"
+                f"{self.aeval_init}"
+            #"0.5*mod(t,1)*lt(mod(t,2),1)*((0.25)-mod(t,(0.25)))*(-1+random(0)*2)"
+            f"expr_inst"
+            "d={duration}\" "
+            f"{self.end}")
+        os.system(expr)
     def render(self):
         self.__eval()
     def play(self):
